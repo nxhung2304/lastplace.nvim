7f457dc74bc7964cc9eccad2512750234a471fb5
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-08-08 22:20:40 -0600

    fix: rename annotation class, shorten lines of code
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/lastplace/config.lua
+++ lua/lastplace/config.lua
@@ -1,32 +1,18 @@
 ---@class lastplace.Config
 local M = {}
 
----@class lastplaceOpts
----@field ignore_filetypes? string[]
----@field ignore_buftypes? string[]
+---@class lastplace.Opts
 ---@field center_on_jump? boolean
----@field jump_only_if_not_visible? boolean
----@field min_lines? integer
----@field max_line? integer
----@field open_folds? boolean
 ---@field debug? boolean
+---@field ignore_buftypes? string[]
+---@field ignore_filetypes? string[]
+---@field jump_only_if_not_visible? boolean
+---@field max_line? integer
+---@field min_lines? integer
+---@field open_folds? boolean
 local default_config = {
-  ignore_filetypes = {
-    "gitcommit",
-    "gitrebase",
-    "svn",
-    "hgcommit",
-    "xxd",
-    "COMMIT_EDITMSG",
-  },
-
-  ignore_buftypes = {
-    "quickfix",
-    "nofile",
-    "help",
-    "terminal",
-  },
-
+  ignore_filetypes = { "COMMIT_EDITMSG", "gitcommit", "gitrebase", "hgcommit", "svn", "xxd" },
+  ignore_buftypes = { "help", "nofile", "quickfix", "terminal" },
   center_on_jump = true,
   jump_only_if_not_visible = false,
   min_lines = 10,
@@ -35,19 +21,19 @@ local default_config = {
   debug = false,
 }
 
-local current_config = {} ---@type lastplaceOpts
+local current_config = {} ---@type lastplace.Opts
 
----@param user_config lastplaceOpts
+---@param user_config lastplace.Opts
 function M.setup(user_config)
-  current_config = vim.tbl_deep_extend("force", default_config, user_config or {}) --[[@as lastplaceOpts]]
+  current_config = vim.tbl_deep_extend("force", default_config, user_config or {}) --[[@as lastplace.Opts]]
 end
 
----@return lastplaceOpts current_config
+---@return lastplace.Opts current_config
 function M.get()
   return current_config
 end
 
----@return lastplaceOpts default_config
+---@return lastplace.Opts default_config
 function M.get_default()
   return default_config
 end
